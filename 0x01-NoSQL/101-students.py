3f73ca36ea9b3fbf1806b97012ec7143e51acefc
YassineRKB <yassine@arcraven.com>
2024-01-24 00:55:54 +0100

    task 14

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 """module for task 14"""
+
+
+def top_students(mongo_collection):
+    """returns all students sorted by average score"""
+    res = mongo_collection.aggregate(
+        [
+            {"$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+            },
+            {"$sort": {"averageScore": -1}}
+        ]
+    )
+    return res
