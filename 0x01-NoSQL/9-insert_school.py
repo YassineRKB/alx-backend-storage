0aad474146ff2d3ca46090a522a6a893b445c555
YassineRKB <yassine@arcraven.com>
2024-01-23 23:56:47 +0100

    task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""module for task 9"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """inserting a document in a collection based on kwargs"""
+    res = mongo_collection.insert_one(kwargs).inserted_id
+    return res
