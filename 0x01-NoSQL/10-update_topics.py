1f5f56ddba3bc472d61234e60def30470decd01e
YassineRKB <yassine@arcraven.com>
2024-01-24 00:09:22 +0100

    task 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""module for task 10"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """updates a document based on name"""
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
