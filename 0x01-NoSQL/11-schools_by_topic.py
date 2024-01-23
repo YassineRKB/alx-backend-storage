e06ab7e49c3fd24aa0f7d2e7e56c8f331b5e2028
YassineRKB <yassine@arcraven.com>
2024-01-24 00:11:53 +0100

    task 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""module for task 11"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """school by topic"""
+    res = mongo_collection.find({"topics": topic})
+    return res
