381c0018ac0895c23d4d35923218b4aea2c79ee4
YassineRKB <yassine@arcraven.com>
2024-01-25 01:22:16 +0100

    task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+"""Module forexpiring web cache and tracker"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """Cache class"""
+
+    def __init__(self):
+        """Constructor method"""
+        self._redis = redis.Redis()
+        self._redis.flushdb(True)
+
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Method that stores cache data"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
